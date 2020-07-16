b66f6f2e1055a0d1246a8fd8bb8147a00401fd56
Jesebell Escobar <jesebellinformatica.gmail.com>
2020-07-16 14:15:22 -0400

    Poliformismo

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,12 @@
 <?php
-require('Account.php');
-require('Car.php');
+require_once("car.php");
+require_once("uberX.php");
+require_once("uberPool.php");
+require_once("account.php");
 
-$car = new Car("LicPHP", new Account("Kam", "7769737"));
-$car->printDataCar();
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
+$uberPool->printDataCar();
+?>
